6f252912e8ec9b872488e6956d0f25789e900249
FrancisDoublewood <tlam102@my.centennialcollege.ca>
2023-05-09 21:22:50 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// const http = require('http'); //import an http module (because need to make http request), just like import library in C#
+// http.createServer((req,res)=>{//create argument obj to store user request and response
+//     res.write('<h1> Welcome to my class </h1>');
+//     res.end(); // must use, otherwise will create infinite loop
+
+// }).listen(8080);//designate the port number to listen to, apache server usually use 8080
+// //in terminal, run npm start --> go to browser localhost:8080 --> show the message
+
+//imports
+const express = require('express');
+const morgan = require('morgan');
+//app init
+const app = express();
+//first route (route=Eg: /main)
+app.get('/main', (req, res)=>{
+    res.json({
+        message: "Welcome to the class"
+    })
+});
+app.listen('8080');
